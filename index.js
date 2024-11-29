f8fc82bbb28e522ed4570a805057ba7792f2d97a
DamienLecointre <89322001+DamienLecointre@users.noreply.github.com>
2024-11-29 10:47:50 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+const burgerMenu = document.querySelector(".burgermenu");
+const submenuList = document.querySelector(".submenu-list");
+
+burgerMenu.addEventListener("click", () => {
+  burgerMenu.classList.toggle("active");
+});
